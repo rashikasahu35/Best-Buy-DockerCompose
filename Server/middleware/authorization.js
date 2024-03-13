c9f97abfe8c9ea0516125a2d1773ee613d56af55
rashikasahu112 <rashikasahu162@gmail.com>
2024-03-13 12:58:01 +0530

    chnages

--- Server/middleware/authorization.js
+++ Server/middleware/authorization.js
@@ -4,12 +4,12 @@ const catchAsyncError = require('./catchAsyncError')
 const jwt = require('jsonwebtoken');
 
 exports.auth = catchAsyncError( async (req, res, next) => {
-    
     const authorizationHeader = req.headers?.authorization;
-    const  { value : token, expiry}  = JSON.parse(authorizationHeader.split(' ')[1])
-    const currentTime = new Date().getTime();
-    
-    if(token && currentTime < expiry){   // if token exist & current time is less than the token expiration time
+    if(!authorizationHeader) throw new Error("Please login to access this resource", 401)
+    console.log(authorizationHeader)
+    const token = JSON.parse(authorizationHeader?.split(' ')[1])?.value
+
+    if(token){   // if token exist
         const { id } = jwt.verify(token, process.env.JWT_SECRET);
         const user = await User.findById(id)
         if(user){
